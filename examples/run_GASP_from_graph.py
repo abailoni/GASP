b5f6e471b5fedb6f40da3ff8c4d2ccf3a434ad2e
Alberto Bailoni <bailoni.alberto@gmail.com>
2019-06-14 13:19:17 +0200

    Fix basic graph example

--- examples/run_GASP_from_graph.py
+++ examples/run_GASP_from_graph.py
@@ -3,32 +3,38 @@ import os
 
 HCI_HOME = "/home/abailoni_local/hci_home"
 sys.path += [
-    os.path.join(HCI_HOME, "python_libraries/nifty/python")]
+    os.path.join(HCI_HOME, "python_libraries/nifty/python"),
+    os.path.join(HCI_HOME, "pyCharm_projects/segmfriends"),
+]
 
 import numpy as np
 
 from nifty.graph import UndirectedGraph
 from GASP.segmentation import run_GASP
 
+# --------------------------
 # Generate a random graph:
-nb_nodes, nb_edges = 1000, 3000
+# --------------------------
+nb_nodes, nb_edges = 7000, 10000
 graph = UndirectedGraph(nb_nodes)
+# Generate some random edges (but avoid self-loop):
 random_edges = np.random.randint(0, nb_nodes - 1, size=(nb_edges, 2))
-# Delete self-loop edges:
-# FIXME: something is going wrong, double edges?
 check = random_edges[:,0] == random_edges[:,1]
-print(check.sum(), random_edges.shape)
-random_edges = np.delete(random_edges, np.argwhere(check)[0], axis=0)
+if any(check):
+    random_edges = np.delete(random_edges, np.argwhere(check), axis=0)
+# Add them to the graph:
 graph.insertEdges(random_edges)
-print(random_edges.shape)
-
-# Run GASP:
+# Generate some random (signed) weights:
 random_signed_weights = np.random.uniform(-1., 1., size=graph.numberOfEdges)
+
+# --------------------------
+# Run GASP:
+# --------------------------
 final_node_labels, runtime = run_GASP(graph,
                                       random_signed_weights,
                                       linkage_criteria='average',
                                       add_cannot_link_constraints=False,
-                                      verbose=True,
+                                      verbose=False,
                                       print_every=100)
 
 print("Clustering took {} s".format(runtime))
